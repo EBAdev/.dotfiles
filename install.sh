01a1e6903de293ff1c6ae08af652249ff71f98df
EBAdev <Emil.aagaard8@gmail.com>
2024-03-05 22:16:24 +0100

    Fixed oh-my-zsh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Create symlinks
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
